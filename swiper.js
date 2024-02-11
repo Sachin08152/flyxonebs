e321f4eb542462183077265f1302884abb1cb83a
Sachin08152 <122515458+Sachin08152@users.noreply.github.com>
2024-02-11 23:44:51 +0530

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,16 @@
+//# sourceMappingURL=bootstrap.min.js.map
+// about1
+var swiper = new swiper(".swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
